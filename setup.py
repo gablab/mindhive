2ae781fe4fbf1ea2dc60a5eb8bfba7baf341c162
Anisha Keshavan <akeshavan@ucla.edu>
2013-03-08 13:39:26 -0500

    fix: added custom env file

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 import os
+import time
 
 files = ['.bashrc','.bash_profile','.projects','.env','.nodeload']
 
 for f in files:
-    if not os.path.exists(os.path.expanduser('~/%s'%f)):
-        cmd = 'ln -s %s ~/'%os.path.abspath(f)
+    if os.path.exists(os.path.expanduser('~/%s'%f)):
+        now = ''.join(time.ctime().split(' ')).replace(':','')
+        newname = '~/%s_%s'%(f,now)
+        cmd = 'mv ~/%s %s'%(f,newname)
         print cmd
-        os.system(cmd)
+        os.system(cmd)   
+    cmd = 'ln -s %s ~/'%os.path.abspath(f)
+    print cmd
+    os.system(cmd)
+ 
+if not os.path.exists(os.path.expanduser('~/.custom_env')):
+    os.system('touch ~/.custom_env')
+
